c82ba9271e850e37f051932bb1893af1d4fa8e24
grafiti-54 <grafiti54@gmail.com>
2024-03-10 14:17:26 +0100

    bdd migration

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from "@/components/modal-provider";
 import { ToasterProvider } from "@/components/toaster-provider";
 import { CrispProvider } from "@/components/crisp-provider";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           <ModalProvider />
           <ToasterProvider/>
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
